3c8b33557f965d3a1c92f15c366e67cb6de9aba9
Sabrina Sulong <tesmemepasla@gmail.com>
2022-02-24 19:31:37 +0800

    🎲 Implement dice roller feature

--- app/src/main/java/com/sabrinasulong/rollerdice/MainActivity.kt
+++ app/src/main/java/com/sabrinasulong/rollerdice/MainActivity.kt
@@ -2,10 +2,28 @@ package com.sabrinasulong.rollerdice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        val imageView: ImageView = findViewById(R.id.imageView)
+
+        button.setOnClickListener {
+            var randomNumber = (1..6).random()
+            val drawableId = when (randomNumber) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            imageView.setImageResource(drawableId)
+        }
     }
 }
